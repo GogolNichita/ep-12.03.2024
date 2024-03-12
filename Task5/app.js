3d8ea45dfe25bd86e5a03af6961b97cc8e68e8a6
GogolNichita <nichitagogol@gmail.com>
2024-03-12 14:46:18 +0200

    Insărcinare nr. 5 dblClick Fixed

--- Task5/app.js
+++ Task5/app.js
@@ -1,41 +1,39 @@
-function decreaseValue(number) {
-    let value = parseInt(input.value);
-    if (value - 10 >= 0) {
-        input.value = value - number;
-    }
+function changeNumberIncrease(number) {
+    clearTimeout(timer);
+
+    timer = setTimeout(function (){
+        let value = parseInt(input.value);
+        if (value + number <= 100) {
+            input.value = value + number;
+        }
+    },150)
 }
 
-function increaseValue(number) {
-    let value = parseInt(input.value);
-    if (value + 10 <= 100) {
-        input.value = value + number;
-    }
+function changeNumberDecrease(number) {
+    clearTimeout(timer);
+
+    timer = setTimeout(function (){
+        let value = parseInt(input.value);
+        if (value - number >= 0) {
+            input.value = value - number;
+        }
+    },150)
 }
 
 const input = document.getElementById('number');
 const decrease = document.getElementById('decrease');
 const increase = document.getElementById('increase');
 let timer;
-decrease.addEventListener('click', (event) => {
-    if (event.detail === 1){
-        timer = setTimeout(()=> {
-            decreaseValue(10)
-        })
-    }
+decrease.addEventListener('click', () => {
+  changeNumberDecrease(10)
 });
-increase.addEventListener('click', (event) => {
-    if (event.detail === 1){
-        timer = setTimeout(()=> {
-            increaseValue(10)
-        })
-    }
+increase.addEventListener('click', () => {
+    changeNumberIncrease(10)
 });
 decrease.addEventListener('dblclick', () =>  {
-    clearTimeout(timer);
-    decreaseValue(5);
+    changeNumberDecrease(5);
 });
 increase.addEventListener('dblclick', () => {
-    clearTimeout(timer);
-    increaseValue(5);
+    changeNumberIncrease(5);
 });
 
